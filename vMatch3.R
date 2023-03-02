5e0a0e6ce5b6d9938bcdd6d17c286bdf92769aa3
samLeung24 <samleung020403@gmail.com>
2023-03-02 11:33:44 +0800

    Applied spacer threshold to the "AGGAGG" consensus seqeunce to alleviate identificaiton accuracy

---
+++ vMatch3.R
@@ -0,0 +1,81 @@
+### À ma chérie? La lapine blanche
+
+library(Biostrings)
+
+# define the SD sequence
+sd_template <- DNAString("AGGAGG")
+
+# read in the fasta file
+fasta_sequences <- readDNAStringSet("~/Desktop/projectRBS/NC_000913.3.fasta")
+
+# extract gene ID and locus name from fasta headers
+header_fields <- strsplit(names(fasta_sequences), "\\|")
+gene_ids <- sapply(header_fields, "[[", 1)
+locus_names <- sapply(header_fields, "[[", 2)
+
+# initialize variables for output
+sd_start <- rep(NA, length(fasta_sequences))
+sd_end <- rep(NA, length(fasta_sequences))
+sd_seq <- rep(NA, length(fasta_sequences))
+spacer_start <- rep(NA, length(fasta_sequences))
+spacer_end <- rep(NA, length(fasta_sequences))
+spacer_length <- rep(NA, length(fasta_sequences))
+spacer_seq <- rep(NA, length(fasta_sequences))
+RBS <- rep(NA, length(fasta_sequences))
+ORF <- rep(NA, length(fasta_sequences))
+
+# loop through each sequence in the fasta file
+for (i in 1:length(fasta_sequences)) {
+  seq <- fasta_sequences[i]
+  seq_length <- length(seq)
+  
+  # adjustable matchmaking threshold
+  sd_matches <- matchPattern(sd_template, substring(seq, 1, 20), max.mismatch = 3, min.mismatch = 0)
+
+  # if SD sequence is found, adjustable spacer threshold
+  if (length(sd_matches) > 0 & (20 - end(sd_matches)[1] == 6 | 
+                                20 - end(sd_matches)[1] == 7 |
+                                20 - end(sd_matches)[1] == 8))  {
+    # get start and end positions of SD sequence
+    sd_start[i] <- start(sd_matches)[1]
+    sd_end[i] <- end(sd_matches)[1]
+    sd_seq[i] <- substring(seq,start(sd_matches)[1],end(sd_matches)[1])
+    
+    # define start and end positions of spacer and the spacer_seq
+    spacer_start[i] <- sd_end[i] + 1
+    spacer_end[i] <- 20
+    spacer_seq[i] <- substring(seq,sd_end[i] + 1, 20)
+    RBS[i] <- substring(seq, 1, 20)
+    ORF[i] <- substring(seq,first = 21)
+    
+    # calculate spacer length
+    spacer_length[i] <- spacer_end[i] - spacer_start[i] + 1
+  } else {
+    # if SD sequence is not found, fill with NAs
+    sd_start[i] <- NA
+    sd_end[i] <- NA
+    sd_seq[i] <- NA
+    spacer_start[i] <- NA
+    spacer_end[i] <- NA
+    spacer_length[i] <- NA
+    spacer_seq[i] <- NA
+    RBS[i] <- substring(seq, 1, 20)
+    ORF[i] <- substring(seq,first = 21)
+  }
+}
+
+# create data frame for output
+output_df <- data.frame(gene_id = gene_ids,
+                        locus_name = locus_names,
+                        sd_start = sd_start,
+                        sd_end = sd_end,
+                        sd_seq = sd_seq,
+                        spacer_start = spacer_start,
+                        spacer_end = spacer_end,
+                        spacer_length = spacer_length,
+                        spacer_seq = spacer_seq,
+                        RBS = RBS,
+                        ORF = ORF)
+
+# write output to csv
+write.csv(output_df, file = "predictionBioString3.csv", row.names = FALSE)
